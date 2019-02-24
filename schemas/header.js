122fe848a952de36129655208a0210bed40d068b
Viktor Bjerre <viktor.bjerre@gmail.com>
2019-02-24 23:17:00 +0100

    adding header

---
+++ schemas/header.js
@@ -0,0 +1,17 @@
+export default {
+  title: "Header",
+  name: "header",
+  type: "document",
+  fields: [
+    {
+      title: "Name",
+      name: "name",
+      type: "string",
+    },
+    {
+      title: "Logo",
+      name: "logo",
+      type: "image",
+    }
+  ]
+}
